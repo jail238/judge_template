db8fcf4696c90729b148f0633c35929ced1d7937
jail238 <90589882+jail238@users.noreply.github.com>
2023-12-25 23:02:07 +0900

    linear_sieve.py

---
+++ math/sieve/linear_sieve.py
@@ -0,0 +1,15 @@
+from math import isqrt
+import sys
+input = sys.stdin.readline
+
+def linear_sieve(n):
+    for i in range(2, n+1):
+        if prime[i] == 1: sieve.append(i)
+        for j in sieve:
+            if i*j > n: break
+            sieve[i*j] = j
+            if i%j == 0: break
+
+n = int(input()) # or input your size
+prime = [0, 0] + [1 for _ in range(n)]
+sieve = []
