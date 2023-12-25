5d1f31ebda94baeb56c0e61f309606cd06aeed93
jail238 <90589882+jail238@users.noreply.github.com>
2023-12-25 23:14:12 +0900

    linear_sieve.py

--- math/sieve/linear_sieve.py
+++ math/sieve/linear_sieve.py
@@ -4,11 +4,25 @@ input = sys.stdin.readline
 
 def linear_sieve(n):
     for i in range(2, n+1):
-        if prime[i] == 1: sieve.append(i)
+        if prime[i] == 1:
+            sieve.append(i)
+            prime[i] = i
         for j in sieve:
             if i*j > n: break
-            sieve[i*j] = j
+            prime[i*j] = j
             if i%j == 0: break
 
-n = int(input())
-prime, sieve = [0, 0] + [1 for _ in range(n)], []
+def divide(n):
+    p = []
+    while n != 1:
+        p.append(prime[n])
+        n //= prime[n]
+    return p
+
+n = int(1e9) # input your problem n size
+prime, sieve = [1 for _ in range(n+2)], []
+linear_sieve(n)
+q = int(input())
+l = list(map(int, input().split()))
+for i in range(q):
+    print(*divide(l[i]))
