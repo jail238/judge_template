7fb9683be39362f5473ffb6708a98399afdeff83
jail238 <90589882+jail238@users.noreply.github.com>
2024-03-19 23:04:51 +0900

    mst.py

--- graph/mst/mst.py
+++ graph/mst/mst.py
@@ -1,11 +1,10 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**5)
 
 def find(p, x):
-    if p[x] != x:
-        p[x] = find(p, p[x])
-    return p[x]
+    while p[x] != x:
+        x = p[x]
+    return x
 
 def union(p, a, b):
     a, b = find(p, a), find(p, b)
