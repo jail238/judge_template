fc13ef60dc7ba2b93eb42c5703e3c8eb8fb1d517
jail238 <jail238@naver.com>
2024-03-30 17:08:22 +0900

    flow.py

---
+++ graph/flow/flow.py
@@ -0,0 +1,44 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, p = map(int, input().split())
+cost = [[0 for _ in range(n+1)] for _ in range(n+1)]
+flow = [[0 for _ in range(n+1)] for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
+
+for _ in range(p):
+    a, b, c = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    cost[a][b] = c # 단방향의 경우
+
+total_flow, s, t = 0, 1, 2
+
+while True:
+    prev = [-1 for _ in range(n+1)]
+    q = deque()
+    q.append(s)
+    while q and prev[t] == -1:
+        now = q.popleft()
+        for nxt in graph[now]:
+            if cost[now][nxt] - flow[now][nxt] > 0:
+                if prev[nxt] == -1:
+                    q.append(nxt)
+                    prev[nxt] = now
+                    if nxt == t: break
+    if prev[t] == -1: break
+    
+    f, i = INF, t
+    while i != s:
+        f = min(f, cost[prev[i]][i]-flow[prev[i]][i])
+        i = prev[i]
+    
+    i = t
+    while i != s:
+        flow[prev[i]][i] += f
+        flow[i][prev[i]] -= f
+        i = prev[i]
+    total_flow += f
+print(total_flow)
