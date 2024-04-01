c292850038ccd7117621f3c273e3e9766d18a30d
jail238 <jail238@naver.com>
2024-04-02 00:26:42 +0900

    matrix_exp_mult.py

--- math/matrix_exp_mult/matrix_exp_mult.py
+++ math/matrix_exp_mult/matrix_exp_mult.py
@@ -1,12 +1,12 @@
-# 2*2 matrix, change 2 to something
+# change 2 to N
 
 import sys
 input = sys.stdin.readline
-mod = 1000
+mod = 998244353
 
 def calc(m1, m2):
     global mod
-    matrix = [[0] * 2 for _ in range(2)]
+    matrix = [[0 for _ in range(2)] for _ in range(2)]
     for i in range(2):
         for j in range(2):
             for k in range(2):
@@ -19,10 +19,11 @@ def func(mat, n):
         return mat
     else:
         if n % 2 == 0:
-            new = func(mat, n // 2)
+            new = func(mat, n//2)
             return calc(new, new)
         else:
             return calc(func(mat, n-1), mat)
 
-matrix = [[1, 0], [0, 1]] 
-func(matrix, n)
+n = int(input())
+matrix = [[1, 1], [1, 0]] 
+x = func(matrix, n-5)
