6826ef0a317fe3f18183ed6828c89c8f3295f901
jail238 <jail238@naver.com>
2024-04-02 12:21:52 +0900

    primality_test.py

--- math/primality_test/primality_test.py
+++ math/primality_test/primality_test.py
@@ -2,8 +2,11 @@ from math import isqrt
 import sys
 input = sys.stdin.readline
 
-def prime(n): # O(N^(1/2))
-    for i in range(2, isqrt(n)+1):
+def prime(n): # O(N^(1/2)/2)
+    if n % 2 == 0:
+        if n == 2: return True
+        else: return False
+    for i in range(3, isqrt(n)+1, 2):
         if n%i == 0:
             return False
     return True
