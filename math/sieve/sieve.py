4b844e11acd6ce1188d9e090481e1cc54ece6b5f
jail238 <jail238@naver.com>
2024-04-02 12:20:15 +0900

    sieve.py

---
+++ math/sieve/sieve.py
@@ -0,0 +1,15 @@
+# O(NloglogN / 2)
+from math import isqrt
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+sieve = [1 for _ in range(n+1)]
+sieve[0] = sieve[1] = 0
+for i in range(4, n+1, 2): sieve[i] = 0
+for i in range(3, isqrt(n)+1):
+    if sieve[i]:
+        j = 2
+        while i*j <= n:
+            sieve[i*j] = 0
+            j += 1
