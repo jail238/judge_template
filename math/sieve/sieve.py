da092f3e9a2a81899f138c2c856fb81e4064be7f
jail238 <jail238@naver.com>
2024-04-23 18:47:38 +0900

    sieve.py

--- math/sieve/sieve.py
+++ math/sieve/sieve.py
@@ -7,7 +7,7 @@ n = int(input())
 sieve = [1 for _ in range(n+1)]
 sieve[0] = sieve[1] = 0
 for i in range(4, n+1, 2): sieve[i] = 0
-for i in range(3, isqrt(n)+1):
+for i in range(3, isqrt(n)+1, 2):
     if sieve[i]:
         j = 2
         while i*j <= n:
