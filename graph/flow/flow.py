7923f2fb197c58603549eb9985ca90f6973b4c59
jail238 <jail238@naver.com>
2024-06-02 23:57:43 +0900

    flow.py

--- graph/flow/flow.py
+++ graph/flow/flow.py
@@ -3,31 +3,32 @@ import sys
 input = sys.stdin.readline
 INF = int(1e9)
 
-n, p = map(int, input().split())
+n, m = map(int, input().split())
 cap = [[0 for _ in range(n+1)] for _ in range(n+1)]
 flow = [[0 for _ in range(n+1)] for _ in range(n+1)]
 graph = [[] for _ in range(n+1)]
 
-for _ in range(p):
+for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
     cap[a][b] = c
 
-total_flow, src, sink = 0, 1, 2
+src, sink = 1, 2
+total_flow = 0
 
 while True:
     prev = [-1 for _ in range(n+1)]
     q = deque()
-    q.append(s)
-    while q and prev[t] == -1:
+    q.append(src)
+    while q and prev[sink] == -1:
         now = q.popleft()
         for nxt in graph[now]:
             if cap[now][nxt] - flow[now][nxt] > 0:
                 if prev[nxt] == -1:
                     q.append(nxt)
                     prev[nxt] = now
-                    if nxt == t: break
+                    if nxt == sink: break
     if prev[sink] == -1: break
     
     f, i = INF, sink
