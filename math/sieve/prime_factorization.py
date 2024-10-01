eeca15603800fa64222542f60a0231c0fb77ec01
jail238 <jail238@naver.com>
2024-10-01 21:23:18 +0900

    prime_factorization.py

--- math/sieve/prime_factorization.py
+++ math/sieve/prime_factorization.py
@@ -1,14 +1,18 @@
 import sys
 input = sys.stdin.readline
 
+def prime_fac(n):
+    div, i, k = {}, 2, n
+    while i*i <= k:
+        while k%i == 0:
+            if not i in div: div[i] = 1
+            else: div[i] += 1
+            k //= i
+        i += 1
+    if k != 1:
+        if not k in div: div[k] = 1
+        else: div[k] += 1
+    return div
+
 n = int(input())
-div, i, k = {}, 2, n
-while i*i <= k:
-    while k%i == 0:
-        if not i in div: div[i] = 1
-        else: div[i] += 1
-        k //= i
-    i += 1
-if k != 1:
-    if not k in div: div[k] = 1
-    else: div[k] += 1
+prime_fac(n)
