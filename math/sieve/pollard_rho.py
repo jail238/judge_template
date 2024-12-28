e9a073a890f8ec28552127c052c5b6b0fdbc29c6
jail238 <jail238@naver.com>
2024-12-28 16:29:02 +0900

    pollard_rho.py

--- math/sieve/pollard_rho.py
+++ math/sieve/pollard_rho.py
@@ -1,9 +1,8 @@
-#BOJ 4149
-
 from math import gcd
 from random import *
 import sys
 input = sys.stdin.readline
+prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
 
 def miller_rabin(n, p):
     if n%p == 0: return True
@@ -19,11 +18,6 @@ def miller_rabin(n, p):
     return False
 
 def prime_chk(n):
-    # for i in range(3): # 2^32
-    #     if n == prime[i]: return True
-    #     if not miller_rabin(n, prime[i]): return False
-    # return True
-
     for i in range(13): # 2^64
         if n == prime[i]: return True
         if not miller_rabin(n, prime[i]): return False
@@ -45,20 +39,16 @@ def pollard_rho(n):
     if prime_chk(d): return d
     else: return pollard_rho(d)
 
-# prime = [2, 7, 61] # n < 2^32
-prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41] # n < 2^64
+def prime_factorization(n):
+    ans = {}
+    while n % 2 == 0:
+        if not 2 in ans: ans[2] = 1
+        else: ans[2] += 1
+        n //= 2
 
-n = int(input())
-ans = []
-
-while n % 2 == 0:
-    n //= 2
-    ans.append(2)
-
-while n > 1:
-    div = pollard_rho(n)
-    ans.append(div)
-    n //= div
-
-for i in sorted(ans):
-    print(i)
+    while n > 1:
+        div = pollard_rho(n)
+        if not div in ans: ans[div] = 1
+        else: ans[div] += 1
+        n //= div
+    return ans
