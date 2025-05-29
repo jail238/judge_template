a50b37646ae278723527e6612ef688e5d7656620
jail238 <jail238@naver.com>
2025-05-29 09:07:38 +0900

    egcd.py

--- math/egcd/egcd.py
+++ math/egcd/egcd.py
@@ -5,3 +5,10 @@ def egcd(a, b):
         m, n = x-s*q, y-t*q
         a, b, x, y, s, t = r, a, s, t, m, n
     return b, x, y #gcd, x0, y0
+
+# ax+by+c = 0이 주어졌을 때 gcd(a, b)와 x0, y0의 실제 값
+a, b, c = map(int, input().split())
+g, x0, y0 = egcd(a, b)
+if -c%g != 0: print(-1); exit() #해가 없음
+tmp = -c//g
+x0, y0 = x0*tmp, y0*tmp
