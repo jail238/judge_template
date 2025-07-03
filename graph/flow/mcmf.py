e2ad3e7ed912c5321984ab09af282840b9a931b0
jail238 <jail238@naver.com>
2025-07-03 14:10:25 +0900

    mcmf.py

--- graph/flow/mcmf.py
+++ graph/flow/mcmf.py
@@ -1,47 +1,59 @@
 from collections import deque
 import sys
 input = sys.stdin.readline
-INF = int(1e9)
+INF = int(1e12)
 
-n, m = map(int, input().split())
-cap = [[0 for _ in range(n+m+3)] for _ in range(n+m+3)] # 용량
-flow = [[0 for _ in range(n+m+3)] for _ in range(n+m+3)] # 유량
-cost = [[0 for _ in range(n+m+3)] for _ in range(n+m+3)] # 비용
-graph = [[] for _ in range(n+m+3)]
+class MCMF:
+    def __init__(self, n):
+        self.n = n
+        self.adj = [[] for _ in range(n+1)]
+        self.inf = INF
+        self.dist, self.visited, self.node, self.edge = [], [], [], []
 
-# write graph information
-total_flow, total_cost, src, sink = 0, 0, 1, 2
+    def add_edge(self, u, v, capacity, cost):
+        self.adj[u].append([v, capacity, cost, len(self.adj[v])])
+        self.adj[v].append([u, 0, -cost, len(self.adj[u])-1])
 
-while True:
-    prev = [-1 for _ in range(n+m+3)]
-    dist = [INF for _ in range(n+m+3)]
-    inq = [False for _ in range(n+m+3)]
-    q = deque(); q.append(src)
-    dist[src] = 0; inq[src] = True
-    
-    while q:
-        now = q.popleft()
-        inq[now] = False
-        for nxt in graph[now]:
-            if cap[now][nxt] - flow[now][nxt] > 0 and cost[now][nxt]+dist[now] < dist[nxt]:
-                dist[nxt] = dist[now]+cost[now][nxt]
-                prev[nxt] = now
-                if not inq[nxt]:
-                    q.append(nxt)
-                    inq[nxt] = True
+    def spfa(self, s, t):
+        self.dist = [self.inf for _ in range((self.n+1))]
+        self.visited = [0 for _ in range((self.n+1))]
+        self.node = [-1 for _ in range((self.n+1))]
+        self.edge = [-1 for _ in range((self.n+1))]
+        q = deque([s])
+        self.dist[s] = 0
+        self.visited[s] = True
 
-    if prev[sink] == -1: break
-    f, i = INF, sink
-    while i != src:
-        f = min(f, cap[prev[i]][i]-flow[prev[i]][i])
-        i = prev[i]
-    i = sink
-    while i != src:
-        total_cost += f*cost[prev[i]][i]
-        flow[prev[i]][i] += f
-        flow[i][prev[i]] -= f
-        i = prev[i]
-    total_flow += f
+        while q:
+            u = q.popleft()
+            self.visited[u] = False
+            for i, (v, cap, cost, _) in enumerate(self.adj[u]):
+                if cap > 0 and self.dist[v]>self.dist[u]+cost:
+                    self.dist[v] = self.dist[u]+cost
+                    self.node[v] = u
+                    self.edge[v] = i
+                    if not self.visited[v]:
+                        q.append(v)
+                        self.visited[v] = 1
+        return self.dist[t] != self.inf
 
-print(total_flow)
-print(total_cost)
+    def solve(self, s, t):
+        max_flow, min_cost = 0, 0
+        while self.spfa(s, t):
+            flow = self.inf
+            now = t
+            while now != s:
+                prev = self.node[now]
+                edge_idx = self.edge[now]
+                flow = min(flow, self.adj[prev][edge_idx][1])
+                now = prev
+            max_flow += flow
+            min_cost += flow*self.dist[t]
+            now = t
+            while now != s:
+                prev = self.node[now]
+                edge_idx = self.edge[now]
+                self.adj[prev][edge_idx][1] -= flow
+                rev_edge_idx = self.adj[prev][edge_idx][3]
+                self.adj[now][rev_edge_idx][1] += flow
+                now = prev
+        return max_flow, min_cost
