037641d6d47eace7191112c02ca34c5276ae5adb
jail238 <jail238@naver.com>
2025-07-07 23:37:16 +0900

    line_intersection.py

--- geometry/line_intersection/line_intersection.py
+++ geometry/line_intersection/line_intersection.py
@@ -12,15 +12,14 @@ def ccw(t1, t2, t3):
     else:
         return -1
 
-x1, y1, x2, y2 = map(int, input().split())
-x3, y3, x4, y4 = map(int, input().split())
-
-if ccw((x1, y1), (x2, y2), (x3, y3))*ccw((x1, y1), (x2, y2), (x4, y4)) == 0 and ccw((x3, y3), (x4, y4), (x1, y1))*ccw((x3, y3), (x4, y4), (x2, y2)) == 0:
-    if min(x1, x2) <= max(x3, x4) and min(x3, x4) <= max(x1, x2) and min(y1, y2) <= max(y3, y4) and min(y3, y4) <= max(y1, y2):
-        print(1)
+def line_inter(p1, p2):
+    x1, y1, x2, y2 = p1; x3, y3, x4, y4 = p2
+    if ccw((x1, y1), (x2, y2), (x3, y3))*ccw((x1, y1), (x2, y2), (x4, y4)) == 0 and ccw((x3, y3), (x4, y4), (x1, y1))*ccw((x3, y3), (x4, y4), (x2, y2)) == 0:
+        if min(x1, x2) <= max(x3, x4) and min(x3, x4) <= max(x1, x2) and min(y1, y2) <= max(y3, y4) and min(y3, y4) <= max(y1, y2):
+            return True
+        else:
+            return False
+    elif ccw((x1, y1), (x2, y2), (x3, y3))*ccw((x1, y1), (x2, y2), (x4, y4)) <= 0 and ccw((x3, y3), (x4, y4), (x1, y1))*ccw((x3, y3), (x4, y4), (x2, y2)) <= 0:
+        return True
     else:
-        print(0)
-elif ccw((x1, y1), (x2, y2), (x3, y3))*ccw((x1, y1), (x2, y2), (x4, y4)) <= 0 and ccw((x3, y3), (x4, y4), (x1, y1))*ccw((x3, y3), (x4, y4), (x2, y2)) <= 0:
-    print(1)
-else:
-    print(0)
+        return False
