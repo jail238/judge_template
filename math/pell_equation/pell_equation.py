716e2561f0cbb0e105a43863d3517df63f7ed4a5
jail238 <jail238@naver.com>
2026-01-15 12:18:07 +0900

    Pell's equation

---
+++ math/pell_equation/pell_equation.py
@@ -0,0 +1,30 @@
+from math import isqrt
+import sys
+input = sys.stdin.readline
+
+def continued_fraction(D):
+    period = [isqrt(D)]
+    m, d, a = 0, 1, period[0]
+    while a != 2*period[0]:
+        m = d*a-m
+        d = (D-m*m)//d
+        a = (period[0]+m)//d
+        period.append(a)
+    return period
+
+def pell_equation(D):
+    sqrtD = isqrt(D)
+    if sqrtD**2 == D: return sqrtD, 1
+    cf = continued_fraction(D)
+    a0, l = cf[0], len(cf)-1
+    if l&1: period = 2*l-1 # 주기 홀수면 -1 다음에 1
+    else: period = l-1
+    pm2, pm1 = 0, 1
+    qm2, qm1 = 1, 0
+    for i in range(period+1):
+        if i == 0: a = cf[0]
+        else: a = cf[(i-1)%l+1]
+        p, q = a*pm1+pm2, a*qm1+qm2
+        pm2, pm1 = pm1, p
+        qm2, qm1 = qm1, q
+    return p, q
